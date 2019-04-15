89f4d4931e9e5515d3a9e8cd3b1bdf34b01912d1
David Lam <Davidlamsgn@gmail.com>
2019-04-15 14:04:00 -0700

    bug fix

--- data/seeds/forest.js
+++ data/seeds/forest.js
@@ -7,58 +7,58 @@ exports.seed = function(knex, Promise) {
       // Inserts seed entries
       return knex('forest').insert([
         {
-          "Entity": "Afghanistan",
-          "Code": "AFG",
-          "Year": 1990,
-          "15.1.1 - Forest area as a proportion of total land area - AG_LND_FRST - Percent (%)": 2.07
+          "entity": "Afghanistan",
+          "code": "AFG",
+          "year": 1990,
+          "percent": 2.07
         },
         {
           "Entity": "Afghanistan",
           "Code": "AFG",
           "Year": 2000,
-          "15.1.1 - Forest area as a proportion of total land area - AG_LND_FRST - Percent (%)": 2.07
+          "percent": 2.07
         },
         {
           "Entity": "Afghanistan",
           "Code": "AFG",
           "Year": 2005,
-          "15.1.1 - Forest area as a proportion of total land area - AG_LND_FRST - Percent (%)": 2.07
+          "percent": 2.07
         },
         {
           "Entity": "Afghanistan",
           "Code": "AFG",
           "Year": 2010,
-          "15.1.1 - Forest area as a proportion of total land area - AG_LND_FRST - Percent (%)": 2.07
+          "percent": 2.07
         },
         {
           "Entity": "Afghanistan",
           "Code": "AFG",
           "Year": 2015,
-          "15.1.1 - Forest area as a proportion of total land area - AG_LND_FRST - Percent (%)": 2.07
+          "percent": 2.07
         },
         {
           "Entity": "Albania",
           "Code": "ALB",
           "Year": 1990,
-          "15.1.1 - Forest area as a proportion of total land area - AG_LND_FRST - Percent (%)": 28.79
+          "percent": 28.79
         },
         {
           "Entity": "Albania",
           "Code": "ALB",
           "Year": 2000,
-          "15.1.1 - Forest area as a proportion of total land area - AG_LND_FRST - Percent (%)": 28.08
+          "percent": 28.08
         },
         {
           "Entity": "Albania",
           "Code": "ALB",
           "Year": 2005,
-          "15.1.1 - Forest area as a proportion of total land area - AG_LND_FRST - Percent (%)": 28.55
+          "percent": 28.55
         },
         {
           "Entity": "Albania",
           "Code": "ALB",
           "Year": 2010,
-          "15.1.1 - Forest area as a proportion of total land area - AG_LND_FRST - Percent (%)": 28.33
+          "percent": 28.33
         }
       ]);
     });
