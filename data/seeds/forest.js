2f017bc1dec18fc1e48067da9030a370cbdabfae
David Lam <Davidlamsgn@gmail.com>
2019-04-15 14:48:56 -0700

    bug fix

--- data/seeds/forest.js
+++ data/seeds/forest.js
@@ -1,65 +1,65 @@
-// import data from '../csvjson.json'
+import data from '../csvjson.json'
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
   return knex('forest').truncate()
     .then(function () {
-      // Inserts seed entries
-      return knex('forest').insert([
-        {
-          "entity": "Afghanistan",
-          "code": "AFG",
-          "year": 1990,
-          "percent": 2.07
-        },
-        {
-          "entity": "Afghanistan",
-          "code": "AFG",
-          "year": 2000,
-          "percent": 2.07
-        },
-        {
-          "entity": "Afghanistan",
-          "code": "AFG",
-          "year": 2005,
-          "percent": 2.07
-        },
-        {
-          "entity": "Afghanistan",
-          "code": "AFG",
-          "year": 2010,
-          "percent": 2.07
-        },
-        {
-          "entity": "Afghanistan",
-          "code": "AFG",
-          "year": 2015,
-          "percent": 2.07
-        },
-        {
-          "entity": "Albania",
-          "code": "ALB",
-          "year": 1990,
-          "percent": 28.79
-        },
-        {
-          "entity": "Albania",
-          "code": "ALB",
-          "year": 2000,
-          "percent": 28.08
-        },
-        {
-          "entity": "Albania",
-          "code": "ALB",
-          "year": 2005,
-          "percent": 28.55
-        },
-        {
-          "entity": "Albania",
-          "code": "ALB",
-          "year": 2010,
-          "percent": 28.33
-        }
-      ]);
+      return knex('forest').insert(data);
+      // return knex('forest').insert([
+      //   {
+      //     "entity": "Afghanistan",
+      //     "code": "AFG",
+      //     "year": 1990,
+      //     "percent": 2.07
+      //   },
+      //   {
+      //     "entity": "Afghanistan",
+      //     "code": "AFG",
+      //     "year": 2000,
+      //     "percent": 2.07
+      //   },
+      //   {
+      //     "entity": "Afghanistan",
+      //     "code": "AFG",
+      //     "year": 2005,
+      //     "percent": 2.07
+      //   },
+      //   {
+      //     "entity": "Afghanistan",
+      //     "code": "AFG",
+      //     "year": 2010,
+      //     "percent": 2.07
+      //   },
+      //   {
+      //     "entity": "Afghanistan",
+      //     "code": "AFG",
+      //     "year": 2015,
+      //     "percent": 2.07
+      //   },
+      //   {
+      //     "entity": "Albania",
+      //     "code": "ALB",
+      //     "year": 1990,
+      //     "percent": 28.79
+      //   },
+      //   {
+      //     "entity": "Albania",
+      //     "code": "ALB",
+      //     "year": 2000,
+      //     "percent": 28.08
+      //   },
+      //   {
+      //     "entity": "Albania",
+      //     "code": "ALB",
+      //     "year": 2005,
+      //     "percent": 28.55
+      //   },
+      //   {
+      //     "entity": "Albania",
+      //     "code": "ALB",
+      //     "year": 2010,
+      //     "percent": 28.33
+      //   }
+      // ]);
     });
 };
