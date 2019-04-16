12aa7f279dcde27ea5097b680dff251705d20d14
David Lam <Davidlamsgn@gmail.com>
2019-04-16 11:10:18 -0700

    bug fix

--- data/migrations/20190416094324_countryinfo.js
+++ data/migrations/20190416094324_countryinfo.js
@@ -3,7 +3,7 @@ exports.up = function(knex, Promise) {
     return knex.schema.createTable("countryinfo", table => {
 		table.string("country");
 		table.string("code");
-        table.integer("year");
+        table.bigInteger("year");
         table.decimal("forest_area_km");
         table.decimal("country_land_area_km");
         table.decimal("forest_propotion_to_land");
