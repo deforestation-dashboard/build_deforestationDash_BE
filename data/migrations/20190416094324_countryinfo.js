a4f24035f01f898fe0e6cbfffc620422473d85ae
David Lam <Davidlamsgn@gmail.com>
2019-04-16 11:29:23 -0700

    bug fix

--- data/migrations/20190416094324_countryinfo.js
+++ data/migrations/20190416094324_countryinfo.js
@@ -4,9 +4,9 @@ exports.up = function(knex, Promise) {
 		table.string("country");
 		table.string("code");
         table.integer("year");
-        table.decimal("forest_area_km", [NULL]);
-        table.decimal("country_land_area_km", [NULL]);
-        table.decimal("forest_propotion_to_land", [NULL]);
+        table.decimal("forest_area_km", null);
+        table.decimal("country_land_area_km", null);
+        table.decimal("forest_propotion_to_land", null);
         table.bigInteger("country_population");
         table.bigInteger("wood_removal");
         table.bigInteger("total_land_usage");
