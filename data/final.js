d22abb2af7af6493f24fc17861cc7a6a2ad78625
David Lam <Davidlamsgn@gmail.com>
2019-04-16 15:18:13 -0700

    bug fix

--- data/final.js
+++
@@ -1,9 +0,0 @@
-// const finaldata = require('../finaldata');
-
-// exports.seed = function(knex, Promise) {
-//   // Deletes ALL existing entries
-//   return knex('deforestation').truncate()
-//     .then(function () {
-//       return knex('deforestation').insert(finaldata);
-//     });
-// };
---
+++ data/seeds/final.js
@@ -0,0 +1,9 @@
+const finaldata = require('../finaldata');
+
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('deforestation').truncate()
+    .then(function () {
+      return knex('deforestation').insert(finaldata);
+    });
+};
