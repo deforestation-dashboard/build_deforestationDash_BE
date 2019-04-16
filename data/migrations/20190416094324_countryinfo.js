956e9207b1b9295959b3b2162c0f3bd7909193d2
David Lam <Davidlamsgn@gmail.com>
2019-04-16 10:52:51 -0700

    bug fix

--- data/migrations/20190416094324_countryinfo.js
+++ data/migrations/20190416094324_countryinfo.js
@@ -9,6 +9,7 @@ exports.up = function(knex, Promise) {
         table.decimal("forest_propotion_to_land");
         table.integer("country_population");
         table.integer("wood_removal");
+        table.integer("total_land_usage");
 	});
 };
 
