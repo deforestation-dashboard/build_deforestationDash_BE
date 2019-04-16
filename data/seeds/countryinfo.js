51e96c047e936bb9bfa02f596db12a015c6ed9cf
David Lam <Davidlamsgn@gmail.com>
2019-04-16 10:41:46 -0700

    bug fix

--- data/seeds/countryinfo.js
+++ data/seeds/countryinfo.js
@@ -2,7 +2,7 @@ const countrydata = require('../countrydata');
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('coutryinfo').truncate()
+  return knex('countryinfo').truncate()
     .then(function () {
       return knex('countryinfo').insert(countrydata);
     });
