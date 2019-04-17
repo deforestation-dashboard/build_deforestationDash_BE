904c0f379366c063da344ae2524deded9ad38e86
David Lam <Davidlamsgn@gmail.com>
2019-04-17 11:48:20 -0700

    new table

--- api/server.js
+++ api/server.js
@@ -4,7 +4,9 @@ const server = express();
 const cors = require("cors");
 const helmet = require("helmet");
 const morgan = require("morgan");
+const axios = require('axios');
 const Forest = require('./model')
+
 
 server.use(express.json());
 server.use(cors());
@@ -12,11 +14,21 @@ server.use(helmet());
 server.use(morgan("dev"));
 
 server.get("/api", (req, res) => {
-	res.status(200).json({ message: "API Live" });
+	let myObj={};
+	const url = 'https://deforestation-dashboard.herokuapp.com/Vietnam/1990'
+	axios.get(url).then(data =>{
+		try {
+			myObj = data.data;
+			res.status(200).json(myObj)
+		} catch (error) {
+			return res.status(500).json({ message: "Error retrieving information" });
+		}
+	}).catch(err=>{
+		return res.status(500).json({ message: "Error retrieving information" });
+	});
 });
 
 server.get("/forest", async (req, res) => {
-
 	try {
 		const list = await Forest.get();
 		res.status(200).json(list);
